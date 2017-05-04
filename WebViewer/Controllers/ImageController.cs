2ebd1208a32997369446fdb616d80528fe7664cf
JohnHu <hjonwy@163.com>
2017-05-04 23:02:06 +0800

    Implement canvas rotate.

--- WebViewer/Controllers/ImageController.cs
+++ WebViewer/Controllers/ImageController.cs
@@ -3,6 +3,7 @@ using Dicom.Imaging;
 using JPACS.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,13 +25,18 @@ namespace WebPACS.Controllers
             List<Image> images = DBHelperFacotry.GetDBHelper().GetImages();
             Image image = images.First<Image>(i => i.Id == id);
 
-            //generate image file
-            DicomImage dcmImage = new DicomImage(image.FilePath);
-
             string imageUrl = "~/Images/" + image.SOPInstanceUid + ".jpg";
             string physicalPath = Server.MapPath(imageUrl);
+            
+            if(!System.IO.File.Exists(physicalPath))
+            {
+                if (!Directory.Exists(Directory.GetParent(physicalPath).FullName))
+                    Directory.CreateDirectory(Directory.GetParent(physicalPath).FullName);
 
-            dcmImage.RenderImage().AsBitmap().Save(physicalPath);
+                //generate image file
+                DicomImage dcmImage = new DicomImage(image.FilePath);
+                dcmImage.RenderImage().AsBitmap().Save(physicalPath);
+            }
 
             ViewBag.ImageUrl = UrlHelper.GenerateContentUrl(imageUrl, ControllerContext.HttpContext);
 
--- tryCanvas/js/app.js
+++ tryCanvas/js/app.js
@@ -2,10 +2,17 @@
 var bCanStartMove = false;
 var c1, ogc, oimg;
 var scale = 1;
+var translate = {x:0, y:0};
+var rotate = 0;
+
+//temp
+var bCircle = false;
+//endtemp
+
 
 window.onload=function(){
 
-	c1 = document.getElementById("c1");
+	c1 = document.getElementById("canvasImage");
 	ogc = c1.getContext('2d');
 	
 	c1.onmousedown = function(evt){
@@ -16,19 +23,21 @@ window.onload=function(){
 	c1.onmouseup = function(evt){
 		c1.style.cursor = "auto";
 		bCanStartMove = false;
-		last_position = {};
+		last_mousePos = {};
 	}
 	
 	c1.onmouseout = function(evt){
 		c1.style.cursor = "auto";
 		bCanStartMove = false;
-		last_position = {};
+		last_mousePos = {};
 	}
 	
-	c1.onmousemove = onCanvasSpan;
+	c1.onmousemove = onCanvasPan;
 	c1.onmousewheel = onCanvasScale;
 	
 	$("#btnRotate").on("click", onCanvasRotate);
+	$("#btnReset").on("click", onCanvasReset);
+	$("#btnCircle").on("click", onDrawCircle);
 	
 	//load image
 	oimg = new Image();
@@ -37,78 +46,61 @@ window.onload=function(){
 		ogc.drawImage(oimg, 0, 0);
 	}
 	
-	oimg.src="img/img1.jpg";
+	oimg.src= imageUrl;
 }
 
-//setup a variable to store our last position
-var last_position = {};
-var last_startPos = {x:0, y:0};
 
 function reDrawImage(){
-	var tWidth = oimg.width * scale;
-	var tHeight = oimg.height *scale;
+	ogc.save();
+	ogc.setTransform(1, 0, 0, 1, 0, 0);
 	
+	//var tWidth = oimg.width * scale;
+	//var tHeight = oimg.height *scale;
 	ogc.clearRect(0, 0, c1.width, c1.height);
-	ogc.drawImage(oimg, 0, 0, oimg.width, oimg.height, last_startPos.x, last_startPos.y, tWidth, tHeight);
+	
+	ogc.translate(translate.x + oimg.width/2, translate.y + oimg.height/2);
+	ogc.rotate(rotate * Math.PI/180);	
+	ogc.scale(scale, scale);
+
+	ogc.drawImage(oimg, 0, 0, oimg.width, oimg.height, -oimg.width/2, -oimg.height/2, oimg.width, oimg.height);
+	
+
+	
+	ogc.restore();
 }
 
 
-//note that `.on()` is new in jQuery 1.7 and is the same as `.bind()` in this case
-function onCanvasSpan(event) {
+var last_mousePos = {};
+
+function onCanvasPan(event) {
 
 	if(!bCanStartMove)
 		return;
 		
-    //check to make sure there is data to compare against
-    if (typeof(last_position.x) != 'undefined') {
+    if (typeof(last_mousePos.x) != 'undefined') {
 
-        //get the change from last position to this position
-        var deltaX = last_position.x - event.clientX,
-            deltaY = last_position.y - event.clientY;
+        var deltaX = last_mousePos.x - event.clientX,
+            deltaY = last_mousePos.y - event.clientY;
 
-        //check which direction had the highest amplitude and then figure out direction by checking if the value is greater or less than zero
-        if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
-            //left
-        } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
-            //right
-        } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
-            //up
-        } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
-            //down
-        }
-        
-        //redraw image
-        //ogc.save(); 
-        
-        last_startPos.x -= deltaX;
-        last_startPos.y -= deltaY;
+        translate.x -= deltaX;
+        translate.y -= deltaY;
 
         reDrawImage();
-        
-        $("#panx").val(last_startPos.x);
-        $("#pany").val(last_startPos.y);
-        
-        //ogc.restore();
-        //ogc.translate();
     }
 	
-    //set the new last position to the current for next time
-    last_position = {
+    last_mousePos = {
         x : event.clientX,
         y : event.clientY
     };
 }
 
 function onCanvasRotate(evt){
-	//ogc.save();
+	rotate += 90;
 	
-//	ogc.translate(-c1.width/2, -c1.height/2);
-//	ogc.rotate(45*Math.PI/180);
-//	
-//	ogc.clearRect(0, 0, c1.width, c1.height);
-//	ogc.drawImage(oimg, last_startPos.x, last_startPos.y);
-	
-	///ogc.restore();
+	if(rotate >= 360)
+		rotate = 0;
+		
+	reDrawImage();
 }
 
 function onCanvasScale(evt){
@@ -116,16 +108,34 @@ function onCanvasScale(evt){
 	if(evt.wheelDelta /120 > 0){
 		//up
 		scale -= 0.1;
-		if(scale <= 0.3){
-			scale = 0.3;
+		if(scale <= 0.1){
+			scale = 0.1;
 		}
 	}
-	else{
+	else{//down
 		scale += 0.1;
-		if(scale >= 1.3){
-			scale = 1.3;
+		if(scale >= 2){
+			scale = 2;
 		}
 	}
 	
 	reDrawImage();
+}
+
+function onCanvasReset(evt){
+	scale = 1;
+    translate = { x: 0, y: 0 };
+    rotate = 0;
+    
+    reDrawImage();
+}
+
+function onDrawCircle(evt){
+	bCircle = true;
+	
+	if(bCircle){
+		jc.start("#canvasImage");
+		jc.circle(100, 100, 200).id('circle1').draggable();
+		jc.start("#canvasImage");
+	}
 }
