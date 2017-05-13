b6e4deb7f62b2d725991e6ae6fb03e64ac4a67f5
JohnHu <hjonwy@163.com>
2017-05-13 22:06:02 +0800

    add 2 canvas demo, annRect is almost done, refactor imageViewer

--- tryCanvas/js/annObjects.js
+++ tryCanvas/js/annObjects.js
@@ -12,59 +12,9 @@
 	workContext.SelectAnnObj= 2;
 	//workContext.Edit = 3;
 
-	var dCanvas;		//the dom canvas object
-	var dImg;			//the dom image object
-	var jcImgLayer;		//the jc image layer object
-	var jcWorkLayer;	//the jc temp working layer
-	
-	var imgLayerId = 'imgLayer';
-	var imgId = 'idImg';
-	
 	//colors
 	var colorWhite = '#ffffff';
 	var colorRed = '#ff0000';
-	
-	//current image viewer working model
-	var curContext = workContext.Pan;
-	
-	var annObjects = new Array();		//the container include all annotation objects
-	var isReady = false;				//indicate whether image is loaded, and ready to draw annotation objects.
-	var curSelectObj = undefined;
-	
-	function selectAnnObject(obj){
-		
-		if(obj && obj instanceof annObject){
-			
-			curSelectObj = obj;
-			
-			//set all other obj not in edit status
-			annObjects.forEach(function(otherObj){
-				if(otherObj !== obj){
-					otherObj.setEdit(false);
-				}
-				else{
-					otherObj.setEdit(true);
-				}
-			});
-		}
-		else{
-			curSelectObj = undefined;
-			
-			annObjects.forEach(function(otherObj){
-				otherObj.setEdit(false);
-			});
-		}
-	}
-	
-	function onImgLayerMousedown(arg){
-		//if in select context, and not click any object, will unselect all objects.
-		if(!arg.event.cancelBubble && curContext == workContext.SelectAnnObj){
-			//selectAnnObject(undefined);
-			if(curSelectObj && curSelectObj.setEdit){
-				curSelectObj.setEdit(false);
-			}
-		}
-	}
 	
 	function screenToImage(x, y, imgTrans){
 	
@@ -96,80 +46,132 @@
 		return {x: screenPt[0], y: screenPt[1]};
 	}
 	
-	var curId = 1;
-	function newId(){
-		curId++;
-		return "annObj_"+curId;
+	var globalViewerId = 1;
+	function newViewerId(){
+		globalViewerId++;
+		return "viewer_" + globalViewerId;
 	}
 	
 	/*********************************
-	 * the annRect class
+	 * the imageViewer class
 	 */
 	
 	function imageViewer(){
-		this.annObjectList = annObjects;
-		this.isReady = isReady;
+		this.id = newViewerId();
+		this.objectList = new Array();
+		this.isReady = false;
+		this.curContext = workContext.Pan;
+		this.curSelectObj = undefined;
+		
+		this.imgLayerId = this.id +'_imgLayer';
+		this.imgId = this.id +'_imgId';
+		
+		this.curObjectId = 0;
+	}
+	
+	imageViewer.prototype.newObjectId = function(){
+		this.curObjectId++;
+		return this.id + "_obj_" + this.curObjectId;	
 	}
 	
 	imageViewer.prototype.initialize = function(canvasId, imgSrc){
-		dCanvas = document.getElementById(canvasId);
+		this.canvas = document.getElementById(canvasId);
 		
-		var imgViewer = this;
+		var iv = this;
 		
-		dImg = new Image();
+		var dImg = new Image();
 		dImg.onload = function(){
 			jc.start(canvasId, true);
 			
-			jcImgLayer = jc.layer(imgLayerId).down('bottom');
-			jcImgLayer.mousedown(onImgLayerMousedown);
+			iv.imgLayer = jc.layer(iv.imgLayerId).down('bottom');
+			iv.imgLayer.mousedown(function(arg){iv.onMouseDown.call(iv, arg)});
 			
-			jc.image(dImg).id(imgId).layer(imgLayerId);
+			jc.image(iv.dImg).id(iv.imgId).layer(iv.imgLayerId);
 			
-			imgViewer.draggable(true);
-			imgViewer.isReady = true;
+			iv.draggable(true);
+			iv.isReady = true;
 			
 			//create objects added before image be loaded
-			if(imgViewer.annObjectList.length > 0){			
-				imgViewer.annObjectList[0].create();
-			}
+			iv.objectList.forEach(function(obj){
+				obj.create();
+			});
 		}
 		
 		dImg.src = imgSrc;
+		this.dImg = dImg;
+	}
+	
+	imageViewer.prototype.selectObject = function(obj){
+		if(obj && obj instanceof annObject){
+			
+			this.curSelectObj = obj;
+			
+			//set all other obj not in edit status
+			this.objectList.forEach(function(otherObj){
+				if(otherObj !== obj){
+					otherObj.setEdit(false);
+				}
+				else{
+					otherObj.setEdit(true);
+				}
+			});
+		}
+		else{
+			this.curSelectObj = undefined;
+			
+			this.objectList.forEach(function(otherObj){
+				otherObj.setEdit(false);
+			});
+		}
+	}
+	
+	imageViewer.prototype.onMouseDown = function(arg){
+		//if in select context, and not click any object, will unselect all objects.
+		if(!arg.event.cancelBubble && this.curContext == workContext.SelectAnnObj){
+			//selectAnnObject(undefined);
+			if(this.curSelectObj && this.curSelectObj.setEdit){
+				this.curSelectObj.setEdit(false);
+			}
+		}
 	}
 	
 	imageViewer.prototype.draggable = function(draggable){
-
-		jcImgLayer.draggable({
+		
+		var iv = this;
+		var canvas = this.canvas;
+		
+		this.imgLayer.draggable({
 			disabled: !draggable,
-			drag: this.onDragImage ? this.onDragImage : function(arg){},
+			drag: iv.onDragImage ? iv.onDragImage : function(arg){},
 			start: function(arg){
-				dCanvas.style.cursor = "move";
+				canvas.style.cursor = "move";
 			},
 			stop: function(arg){
-				dCanvas.style.cursor = "default";
+				canvas.style.cursor = "default";
 			}
 		});
 	}
 	
 	imageViewer.prototype.setSelectModel = function(){
-		curContext = workContext.SelectAnnObj;
+		this.curContext = workContext.SelectAnnObj;
 		
 		this.draggable(false);
-		//dCanvas.style.cursor = 'pointer';
+		//canvas.style.cursor = 'pointer';
 	}
 	
 	imageViewer.prototype.setPanModel = function(){
-		curContext = workContext.Pan;
+		this.curContext = workContext.Pan;
 		
 		this.draggable(true);
-		//dCanvas.style.cursor = 'default';
+		//canvas.style.cursor = 'default';
 		
-		selectAnnObject(undefined);
+		this.selectObject(undefined);
 	}
 	
 	imageViewer.prototype.addRect = function(x, y, width, height){
 		var aRect = new annRect(x, y, width, height);
-		this.annObjectList.push(aRect);
+		aRect.parent = this;
+		this.objectList.push(aRect);
 		
 		if(this.isReady){
 			aRect.create();
@@ -179,14 +181,14 @@
 	}
 	
 	/*********************************
-	 * the annRect class
+	 * the annObject class
 	 */
 
 	function annObject(){
+		this.parent = undefined;
 		this.isInEdit = false;
 		this.id = "";
 	}
-	
 	
 	/*********************************
 	 * the annRect class
@@ -199,32 +201,89 @@
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		
+		//for asisit
+		this._lastRectPos = {};
+		this._lastCirclePos = {};
 	}
 	
 	annRect.prototype = new annObject();
 	
 	annRect.prototype.create = function(){
-		var rectId = newId();
+		var iv = this.parent;
+		
+		//draw rect
+		var rectId = iv.newObjectId();
 		this.id = rectId;
 		
-		jc.rect(this.x, this.y, this.width, this.height).layer(imgLayerId).id(rectId).color(colorWhite);
-		this.jcRect = jc('#'+ rectId);
+		jc.rect(this.x, this.y, this.width, this.height).layer(iv.imgLayerId).id(rectId).color(colorWhite);
+		this.rect = jc('#'+ rectId);
+		//this.rect.lineStyle({lineWidth:1});
 		
+		//handle rect events
 		var aRect = this;
 		
-		this.jcRect.mouseover(function(){
-			if(curContext == workContext.SelectAnnObj)
-				dCanvas.style.cursor = 'pointer';
+		this.rect.mouseover(function(){
+			if(iv.curContext == workContext.SelectAnnObj)
+				iv.canvas.style.cursor = 'pointer';
 		});
 		
-		this.jcRect.mouseout(function(){
-			if(curContext == workContext.SelectAnnObj)
-				dCanvas.style.cursor = 'default';
+		this.rect.mouseout(function(){
+			if(iv.curContext == workContext.SelectAnnObj)
+				iv.canvas.style.cursor = 'default';
 		});
 		
-		this.jcRect.mousedown(function(arg){
-			if(curContext == workContext.SelectAnnObj){
-				selectAnnObject(aRect);
+		this.rect.mousedown(function(arg){
+			if(iv.curContext == workContext.SelectAnnObj){
+				iv.selectObject(aRect);
+				
+				arg.event.cancelBubble = true;
+			}
+		});
+		
+		//draw label text
+		var idLbl = this.id+"_lbl";
+		var lblPos = {x:this.x+5, y:this.y-5};
+		jc.text('办证' +idLbl, lblPos.x, lblPos.y).id(idLbl).layer(iv.imgLayerId).color(colorWhite).font('15px Times New Roman');
+		this.label = jc('#'+idLbl);
+		
+		this.label.mouseover(function(){
+			if(iv.curContext == workContext.SelectAnnObj)
+				iv.canvas.style.cursor = 'pointer';
+		});
+		
+		this.label.mouseout(function(){
+			if(iv.curContext == workContext.SelectAnnObj)
+				iv.canvas.style.cursor = 'default';
+		});
+		
+		this.label.mousedown(function(arg){
+			if(iv.curContext == workContext.SelectAnnObj){
+				iv.selectObject(aRect);
+				
+				arg.event.cancelBubble = true;
+			}
+		});
+		
+		//draw assit objects
+		var idCircleA = this.id+"_aCircle";
+		jc.circle(this.x, this.y, 5).id(idCircleA).layer(iv.imgLayerId).color(colorRed);
+		this.circleA = jc("#"+idCircleA);
+		this.circleA.visible(false);
+		
+		this.circleA.mouseover(function(){
+			if(iv.curContext == workContext.SelectAnnObj)
+				iv.canvas.style.cursor = 'nw-resize';
+		});
+		
+		this.circleA.mouseout(function(){
+			if(iv.curContext == workContext.SelectAnnObj)
+				iv.canvas.style.cursor = 'default';
+		});
+		
+		this.circleA.mousedown(function(arg){
+			if(iv.curContext == workContext.SelectAnnObj){
+				iv.selectObject(aRect);
 				
 				arg.event.cancelBubble = true;
 			}
@@ -233,26 +292,115 @@
 	
 	annRect.prototype.setEdit = function(edit){
 		this.isInEdit = edit;
+		this.setDraggable(edit);
+		this.circleA.visible(edit);
 		
 		if(edit){
-			this.jcRect.color(colorRed);
-			this.setDraggable(true);
-			
-			//draw edit assit objects
-			//this.jcStartCircle = undefined;
-			//this.jcRect = undefined;
-			//this.jcLable = undefined;
+			this.rect.color(colorRed);
+			this.label.color(colorRed);
 		}
 		else{
-			this.jcRect.color(colorWhite);
-			this.setDraggable(false)
+			this.rect.color(colorWhite);
+			this.label.color(colorWhite);
 		}
 	}
 	
 	annRect.prototype.setDraggable = function(draggable){
-		this.jcRect.draggable({disabled: !draggable});
-	}
+		var aRect = this;
+		var transTmp = this.parent.imgLayer.transform();
+		
+		this.rect.draggable({
+			disabled: !draggable,
+			start: function(arg){
+				this._lastPos = {};
+			},
+			stop: function(arg){
+				this._lastPos = {};
+			},
+			drag: function(arg){
+				var ptImg = screenToImage(arg.x, arg.y, transTmp);
+				
+				if (typeof(this._lastPos.x) != 'undefined') {
 	
+					var deltaX = ptImg.x - this._lastPos.x;
+					var deltaY = ptImg.y - this._lastPos.y;
+					
+					this.translate(deltaX, deltaY);
+					aRect.circleA.translate(deltaX, deltaY);
+					aRect.label.translate(deltaX, deltaY);
+				}
+				
+				this._lastPos = {
+					x: ptImg.x,
+					y: ptImg.y
+				};
+				
+				return true;
+			}
+		});
+		
+		this.circleA.draggable({
+			disabled: !draggable,
+			start: function(arg){
+				this._lastPos = {};
+			},
+			stop: function(arg){
+				this._lastPos = {};
+			},
+			drag:function(arg){
+				var ptImg = screenToImage(arg.x, arg.y, transTmp);
+				
+				if (typeof(this._lastPos.x) != 'undefined') {
+	
+					var deltaX = ptImg.x - this._lastPos.x;
+					var deltaY = ptImg.y - this._lastPos.y;
+					
+					this.translate(deltaX, deltaY);
+					
+					aRect.rect.translate(deltaX, deltaY);
+					aRect.rect._width -= deltaX;
+					aRect.rect._height -= deltaY;
+					
+					aRect.label.translate(deltaX, deltaY);
+				}
+				
+				this._lastPos = {
+					x: ptImg.x,
+					y: ptImg.y
+				};
+				
+				return true;
+			}
+		});
+		
+		this.label.draggable({
+			disabled: !draggable,
+			start: function(arg){
+				this._lastPos = {};
+			},
+			stop: function(arg){
+				this._lastPos = {};
+			},
+			drag:function(arg){
+				var ptImg = screenToImage(arg.x, arg.y, transTmp);
+				
+				if (typeof(this._lastPos.x) != 'undefined') {
+	
+					var deltaX = ptImg.x - this._lastPos.x;
+					var deltaY = ptImg.y - this._lastPos.y;
+					
+					this.translate(deltaX, deltaY);
+				}
+				
+				this._lastPos = {
+					x: ptImg.x,
+					y: ptImg.y
+				};
+				
+				return true;
+			}
+		});
+	}
 	
 	/*********************************
 	 * the annLine class
